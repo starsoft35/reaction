539c3036fdfa126ad70bda77791e306b4301e311
Mike Murray <hellomikemurray@gmail.com>
2019-10-25 10:26:57 -0700

    feat: allow array of category IDs
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- src/plugins/category/resolvers/Query/productsByCategoryIds.js
+++ src/plugins/category/resolvers/Query/productsByCategoryIds.js
@@ -1,10 +1,10 @@
 import { decodeShopOpaqueId, decodeCategoryOpaqueId } from "../../xforms/id.js";
 
 /**
- * @name Query.productsByCategoryId
+ * @name Query.productsByCategoryIds
  * @method
  * @memberof Categories/GraphQL
- * @summary get a list of products by category id
+ * @summary get a list of products by category ids
  * @param {Object} _ - unused
  * @param {Object} [params] - an object of all arguments that were sent by the client
  * @param {String} [params.shopId] - Shop id
@@ -12,29 +12,31 @@ import { decodeShopOpaqueId, decodeCategoryOpaqueId } from "../../xforms/id.js";
  * @param {Object} context - an object containing the per-request state
  * @returns {Promise<Array<Object>>} CategoryProducts Connection
  */
-export default async function productsByCategoryId(_, params, context) {
+export default async function productsByCategoryIds(_, params, context) {
   const {
     after,
     before,
     first,
     last,
+    offset,
     shopId: opaqueShopId,
     sortOrder,
-    categoryId: opaqueCategoryId
+    categoryIds: opaqueCategoryIds
   } = params;
 
   const shopId = decodeShopOpaqueId(opaqueShopId);
-  const categoryId = decodeCategoryOpaqueId(opaqueCategoryId);
+  const categoryIds = opaqueCategoryIds.map(decodeCategoryOpaqueId);
 
-  return context.queries.productsByCategoryId(context, {
+  return context.queries.productsByCategoryIds(context, {
     connectionArgs: {
       after,
       before,
       first,
       last,
+      offset,
       sortOrder
     },
     shopId,
-    categoryId
+    categoryIds
   });
 }
